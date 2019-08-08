bf4bea7b080d7a4e95e461d76186aefd50318ca8
justworld <jiangui1376044@126.com>
2019-08-08 10:12:14 +0800

    fix bug

--- server_study/django_app/apps/__init__.py
+++ server_study/django_app/apps/__init__.py
@@ -1,9 +1,4 @@
 # coding: utf-8
 from rest_framework.routers import SimpleRouter
 
-from apps.hello_world.apis import HelloViewSet
-
 api_router = SimpleRouter()
-
-# register
-api_router.register(r'hello', HelloViewSet, basename='hello')
--- server_study/django_app/settings/urls.py
+++ server_study/django_app/settings/urls.py
@@ -2,6 +2,11 @@
 from django.conf.urls import include, url
 from apps import api_router
 
+# register
+from apps.hello_world.apis import HelloViewSet
+
+api_router.register(r'hello', HelloViewSet, basename='hello')
+
 urlpatterns = [
     url(r'^api/', include(api_router.urls))
 ]
