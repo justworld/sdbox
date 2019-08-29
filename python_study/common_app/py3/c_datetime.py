e94f1607e336c4a13acfc0398fc27daf9d88388d
justworld <jiangui1376044@126.com>
2019-08-29 16:45:21 +0800

    timezone

--- python_study/common_app/py3/c_datetime.py
+++ python_study/common_app/py3/c_datetime.py
@@ -1,1 +1,9 @@
-#coding: utf-8
+# coding: utf-8
+# 时区相关操作
+from datetime import datetime
+from pytz import timezone
+
+now = datetime.now()
+tz = timezone('America/Ensenada')
+now_1 = now.replace(tzinfo=tz)
+now.astimezone()
